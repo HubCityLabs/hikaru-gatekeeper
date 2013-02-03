4e8687078401b38dc17a1f69574621e18960b97f
Jean-Francois Arseneau <jf.arseneau@gmail.com>
2013-02-02 21:13:24 -0400

    Timestamping added

--- src/gatekeeper.py
+++ src/gatekeeper.py
@@ -1,6 +1,9 @@
 import subprocess
 import json
 import requests
+import time
+
+print "Gatekeeper, ENGAGED!"
 
 # Scan forever and ever and ever and ever
 while True:
@@ -9,11 +12,19 @@ while True:
 '/home/pi/hikaru-gatekeeper/src/poll.py'])
     print "Card scanned!"
     print results
+    # Store the timestamp to then use it to hash
+    timestamp = time.time()
+    print "Timestamp: " + str(timestamp)
 
     # Convert the JSON into a list, then send it to Hikaruspace
     scan_results = json.loads(results)
     r = requests.get('http://moncton.zombievolk.com:9999/update_clients',
             params={'reader_id':scan_results['reader_id'], 
                     'card_uid':scan_results['card_uid'],
-                    'status':scan_results['status']})
+                    'status':scan_results['status'],
+                    'timestamp':timestamp})
+
+    card_uid = scan_results['card_uid']
+
     print "Triggering " + r.url
+    print "Response: " + r.text
